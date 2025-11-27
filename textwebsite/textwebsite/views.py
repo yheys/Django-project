8717ff586a58893ea6965007b6d18d43b5ce2f60
yheys <yheyskebede2015@gmail.com>
2025-11-28 00:48:16 +0300

    edited django

--- textwebsite/textwebsite/views.py
+++ textwebsite/textwebsite/views.py
@@ -1,8 +1,47 @@
 # i created this file
+from email.policy import default
 
 from django.http import HttpResponse
+from django.shortcuts import render
+
 def index (request):
-    return HttpResponse("hi yheys")
+    pars={"name":"yheys","place": "addis ababa"}
+    return render(request,"index.html", pars)
 
 def about (request):
     return HttpResponse("<h1>yheys</h1>"  "<h2>yheys</h2>" )
+def analyze(request):
+    djangotext=request.GET.get("text","default")
+    removingpunc = request.GET.get("remocingpunc", "off")
+    print(removingpunc)
+    print(djangotext)
+    if removingpunc=="on":
+        #analyzed=djangotext
+        punctuations="""!(){}[],.:;'"\/<>?#$%^&*_-"""
+        analyzed= ""
+        for char in djangotext:
+            if char not in punctuations:
+                analyzed+=char
+
+        params={"purpose": "remove the punctuations","analyzed_text": analyzed}
+        return render(request,"analyze.html",params)
+    # else:
+    #     return HttpResponse("error")
+
+
+
+
+
+
+
+
+
+
+# def capitalzingfirst (request):
+#     return HttpResponse("hiusbdjk")
+# def newlineremover (request):
+#     return HttpResponse("newline remove")
+# def thespaceremover (request):
+#     return HttpResponse("remove the space<a href='/'> back</a>")
+# def charactercount (request):
+#     return HttpResponse("count the character")
